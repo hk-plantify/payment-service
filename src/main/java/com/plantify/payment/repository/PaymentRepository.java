709377be79f1cd3c682aeee5eb2751cf4253414b
zzoo0123 <0123suh@gmail.com>
2024-12-02 02:13:13 +0900

    feat: Payment service

--- src/main/java/com/plantify/payment/repository/PaymentRepository.java
+++ src/main/java/com/plantify/payment/repository/PaymentRepository.java
@@ -1,13 +1,16 @@
 package com.plantify.payment.repository;
 
 import com.plantify.payment.domain.entity.Payment;
-import com.plantify.payment.domain.entity.Status;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
-    List<Payment> findByStatus(Status status);
     List<Payment> findByUserId(Long userId);
+    Page<Payment> findByUserId(Long userId, Pageable pageable);
+    Optional<Payment> findByPaymentIdAndUserId(Long paymentId, Long userId);
 }
---
+++ src/main/java/com/plantify/payment/service/PaymentUserService.java
@@ -0,0 +1,6 @@
+package com.plantify.payment.service;
+
+
+public interface PaymentUserService {
+
+}
