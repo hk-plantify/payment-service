de5a15bccf6ac22e12bf6f2a667b30b07aee4f18
zzoo0123 <0123suh@gmail.com>
2024-12-07 16:22:23 +0900

    feat: global exception, response 수정

--- src/main/java/com/plantify/payment/global/response/ApiResponse.java
+++ src/main/java/com/plantify/payment/global/response/ApiResponse.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public class ApiResponse<T> {
+
     private Integer status;
     private String message;
     private T data;
