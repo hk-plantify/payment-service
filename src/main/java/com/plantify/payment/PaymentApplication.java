86aee50e08dc0727fbf1500bfa7e7d862d9b1c44
zzoo0123 <0123suh@gmail.com>
2024-12-07 16:22:52 +0900

    feat: converter 추가

--- src/main/java/com/plantify/payment/PaymentApplication.java
+++ src/main/java/com/plantify/payment/PaymentApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.support.converter.JsonMessageConverter;
+import org.springframework.kafka.support.converter.RecordMessageConverter;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableFeignClients
@@ -11,6 +14,11 @@ public class PaymentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentApplication.class, args);
+	}
+
+	@Bean
+	public RecordMessageConverter converter() {
+		return new JsonMessageConverter();
 	}
 
 }
